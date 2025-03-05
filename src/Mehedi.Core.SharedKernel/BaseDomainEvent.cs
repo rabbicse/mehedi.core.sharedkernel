b70ce65f0f374ab0a8fd75bfde7f71bca07795e1
Md Mehedi Hasan <rabbi.se@gmail.com>
2025-03-05 14:00:51 +0600

    Update BaseDomainEvent

--- src/Mehedi.Core.SharedKernel/BaseDomainEvent.cs
+++ src/Mehedi.Core.SharedKernel/BaseDomainEvent.cs
@@ -3,20 +3,23 @@
 /// <summary>
 /// Represents a base event.
 /// </summary>
-public abstract record BaseDomainEvent : IDomainEvent
+#pragma warning disable CA1708 // Identifiers should differ by more than case
+public abstract record BaseDomainEvent(string? messageType, Guid aggregateId) : IDomainEvent
+#pragma warning restore CA1708 // Identifiers should differ by more than case
 {
     /// <summary>
     /// Gets the type of the message.
     /// </summary>
-    public string? MessageType { get; protected init; }
+    public string? MessageType { get; protected init; } = messageType;
 
     /// <summary>
     /// Gets the aggregate ID.
     /// </summary>
-    public Guid AggregateId { get; protected init; }
+    public Guid AggregateId { get; protected init; } = aggregateId;
 
     /// <summary>
     /// Gets the date and time when the event occurred.
     /// </summary>
     public DateTime OccurredOn { get; private init; } = DateTime.UtcNow;
+
 }
